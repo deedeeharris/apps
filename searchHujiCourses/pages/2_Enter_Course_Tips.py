7034be32dcf91f407f262b5a14fb557a3f8483b7
Yedidya Harris <80841344+deedeeharris@users.noreply.github.com>
2022-12-11 22:53:03 +0200

    Add files via upload

---
+++ searchHujiCourses/pages/2_Enter_Course_Tips.py
@@ -0,0 +1,7 @@
+import streamlit.components.v1 as components 
+import streamlit as st
+
+components.html("""<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSet4jdiW4tw3U9VOdD7UL9vLg_rM3XQXAaFrdz_GYGsfyEVxg/viewform" 
+width="900" height="1500">
+</iframe>
+""", width=800, height=500)
