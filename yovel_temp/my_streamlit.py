fbc27c35cc7c039c61085e7a77e990ceff222b66
Yedidya Harris <80841344+deedeeharris@users.noreply.github.com>
2022-12-28 13:19:00 +0200

    Update my_streamlit.py

--- yovel_temp/my_streamlit.py
+++ yovel_temp/my_streamlit.py
@@ -129,9 +129,6 @@ st.sidebar.image(image)
 segmented_image = otsu_segment(image, sigma=sigma_value)
 
 
-# test the output image
-st.text(type(segmented_image))
-st.text(segmented_image)
 # Display the result on the right (main frame)
 st.subheader('Output Image')
 st.image(segmented_image)
