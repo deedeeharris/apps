1486d0b40b8a311b6dcc676d3981270c10285ce1
Yedidya Harris <80841344+deedeeharris@users.noreply.github.com>
2022-12-31 22:51:50 +0200

    Update img_filters.py

--- img_filters.py
+++ img_filters.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from PIL import Image, ImageFilter
+import cv2
+import numpy as np
 
 st.markdown("# Image Filter App")
 st.markdown("Please upload an image using the button below and choose one of the filters from the dropdown menu to apply to the image. Use the sliders to adjust the filter parameters as needed.")
@@ -7,7 +8,9 @@ st.markdown("Please upload an image using the button below and choose one of the
 uploaded_file = st.file_uploader("Choose an image file", type="jpg")
 
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
+    # Load the image using OpenCV
+    image = cv2.imread(uploaded_file)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     
     st.image(image, caption="Original image", use_column_width=True)
     
@@ -16,29 +19,26 @@ if uploaded_file is not None:
     
     # Sharpen filter has no adjustable parameters
     if filter_choice == "Sharpen":
-        filtered_image = image.filter(ImageFilter.SHARPEN)
+        kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+        filtered_image = cv2.filter2D(image, -1, kernel)
     
     # Canny edge detection filter has two adjustable parameters: sigma and lower/upper threshold
     elif filter_choice == "Canny edge detection":
         sigma = st.slider("Sigma", 0.0, 3.0, 0.5, 0.1)
         lower_threshold = st.slider("Lower threshold", 0, 255, 50)
         upper_threshold = st.slider("Upper threshold", 0, 255, 150)
-        filtered_image = image.filter(ImageFilter.Canny(sigma=float(sigma), lower=int(lower_threshold), upper=int(upper_threshold)))
+        filtered_image = cv2.Canny(image, lower_threshold, upper_threshold, sigma)
     
     # Median filter has one adjustable parameter: size
     elif filter_choice == "Median filter":
         size = st.slider("Size", 3, 15, 3)
-        filtered_image = image.filter(ImageFilter.MedianFilter(size=size))
+        filtered_image = cv2.medianBlur(image, size)
     
-    # Blur filter has one adjustable parameter: radius
+    # Blur filter has one adjustable parameter: kernel size
     elif filter_choice == "Blur":
-        radius = st.slider("Radius", 0, 10, 2)
-        filtered_image = image.filter(ImageFilter.BLUR(radius=radius))
-    
-    # Contour filter has one adjustable parameter: scale
-    elif filter_choice == "Contour":
-        scale = st.slider("Scale", 0, 10, 1)
-        filtered_image = image.filter(ImageFilter.CONTOUR(scale=scale))
+        kernel_size = st.slider("Kernel size", 3, 15, 3)
+        filtered_image = cv2.blur
+
     
     # Display the filtered image only after the user has chosen the values from the sliders
     if st.button("Apply"):
