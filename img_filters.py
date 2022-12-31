4759a620a806377504c86319cff24d16d1dcb692
Yedidya Harris <80841344+deedeeharris@users.noreply.github.com>
2022-12-31 22:48:15 +0200

    Update img_filters.py

--- img_filters.py
+++ img_filters.py
@@ -14,24 +14,32 @@ if uploaded_file is not None:
     filters = ["Sharpen", "Canny edge detection", "Median filter", "Blur", "Contour"]
     filter_choice = st.selectbox("Choose a filter", filters)
     
+    # Sharpen filter has no adjustable parameters
     if filter_choice == "Sharpen":
-        # Sharpen filter has no adjustable parameters
         filtered_image = image.filter(ImageFilter.SHARPEN)
+    
+    # Canny edge detection filter has two adjustable parameters: sigma and lower/upper threshold
     elif filter_choice == "Canny edge detection":
-        # Canny edge detection filter has two adjustable parameters: sigma and lower/upper threshold
         sigma = st.slider("Sigma", 0.0, 3.0, 0.5, 0.1)
         lower_threshold = st.slider("Lower threshold", 0, 255, 50)
         upper_threshold = st.slider("Upper threshold", 0, 255, 150)
-        filtered_image = image.filter(ImageFilter.Canny(sigma=sigma, lower=lower_threshold, upper=upper_threshold))
+        filtered_image = image.filter(ImageFilter.Canny(sigma=float(sigma), lower=int(lower_threshold), upper=int(upper_threshold)))
+    
+    # Median filter has one adjustable parameter: size
     elif filter_choice == "Median filter":
-        # Median filter has one adjustable parameter: size
         size = st.slider("Size", 3, 15, 3)
         filtered_image = image.filter(ImageFilter.MedianFilter(size=size))
-    elif filter_choice == "Blur":
-        # Blur filter has no adjustable parameters
-        filtered_image = image.filter(ImageFilter.BLUR)
-    elif filter_choice == "Contour":
-        # Contour filter has no adjustable parameters
-        filtered_image = image.filter(ImageFilter.CONTOUR)
     
-    st.image(filtered_image, caption=filter_choice, use_column_width=True)
+    # Blur filter has one adjustable parameter: radius
+    elif filter_choice == "Blur":
+        radius = st.slider("Radius", 0, 10, 2)
+        filtered_image = image.filter(ImageFilter.BLUR(radius=radius))
+    
+    # Contour filter has one adjustable parameter: scale
+    elif filter_choice == "Contour":
+        scale = st.slider("Scale", 0, 10, 1)
+        filtered_image = image.filter(ImageFilter.CONTOUR(scale=scale))
+    
+    # Display the filtered image only after the user has chosen the values from the sliders
+    if st.button("Apply"):
+        st.image(filtered_image, caption=filter_choice, use_column_width=True)
