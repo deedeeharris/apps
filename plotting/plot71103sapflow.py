626eb6407723801359ef3db218be9f60e7ed25ec
Yedidya Harris <80841344+deedeeharris@users.noreply.github.com>
2023-01-04 15:26:13 +0200

    Update plot71103sapflow.py

--- plotting/plot71103sapflow.py
+++ plotting/plot71103sapflow.py
@@ -1,8 +1,8 @@
 import streamlit as st
-from streamlit.components.html import HTML
+
 
 
 html_url = "https://raw.githubusercontent.com/deedeeharris/apps/main/plotting/time_series.html"
 
 iframe = '<iframe src="{}" width="800" height="600"></iframe>'.format(html_url)
-HTML(iframe)
+st.markdown(iframe, unsafe_allow_html=True)
