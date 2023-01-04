dc37e50949de7a926ca9da19fecc0d63d3e200cf
Yedidya Harris <80841344+deedeeharris@users.noreply.github.com>
2023-01-04 15:28:20 +0200

    Delete plot71103sapflow.py

--- plotting/plot71103sapflow.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-
-
-html_url = "https://raw.githubusercontent.com/deedeeharris/apps/main/plotting/time_series.html"
-
-iframe = '<iframe src="{}" width="800" height="600"></iframe>'.format(html_url)
-st.markdown(iframe, unsafe_allow_html=True)
