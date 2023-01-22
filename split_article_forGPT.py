374104f73e231623d7bfdc85ac226a0e99f4fa4d
Yedidya Harris <80841344+deedeeharris@users.noreply.github.com>
2023-01-22 10:41:37 +0200

    Update split_article_forGPT.py

--- split_article_forGPT.py
+++ split_article_forGPT.py
@@ -41,7 +41,7 @@ def main():
         st.success("Text has been splitted!")
         
         st.markdown("1. Send ChatGPT the following prompt:")
-        st.markdown(""""Your name is "Student". Every time you answer me, write "Student: answer". I want you to act as a PhD student and provide summaries of scientific articles that I will provide the full text of. The summaries should be concise and suitable for presentation in a classroom setting as a PowerPoint. The summary should cover the main findings and conclusions of the article, and should be written in proper academic language. You may include specific citations from my article or references in your summaries from my article. Your summary should be free of any personal opinions or bias. My first article will be attached in the following messages. Start summarizing only after you've received the entire article, and that will be when i say "GO". If you start summarizing by accident, without the entire article, I'll say "WAIT". Everytime i send a message, reply with "You've just sent [x] number of words, send the rest!". "
+        st.markdown(""""Your name is "Student". Every time you answer me, write "Student: answer". I want you to act as a PhD student and provide summaries of scientific articles that I will provide the full text of. The summaries should be concise and suitable for presentation in a classroom setting as a PowerPoint. The summary should cover the main findings and conclusions of the article, and should be written in proper academic language. You may include specific citations from my article or references in your summaries from my article. Your summary should be free of any personal opinions or bias. My first article will be attached in the following messages. Start summarizing only after you've received the entire article, and that will be when i say "GO". If you start summarizing by accident, without the entire article, I'll say "WAIT". Everytime i send you a message, reply with "You've just sent [x] number of words, send the rest!". x in the brackets, should be the number of words i just sent. "
                     """)
         st.markdown("2. Send each part of the article, in one message.")
 
@@ -50,6 +50,7 @@ def main():
             file_text = f.read()
             st.text(file_text)
         
+        st.text('When you finish sending all the parts, just write GPT to "GO".')
 
 
 if __name__ == '__main__':
