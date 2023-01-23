a9e377781c2c28134ab4ffa24d5a5394a6caba13
Yedidya Harris <80841344+deedeeharris@users.noreply.github.com>
2023-01-23 20:16:44 +0200

    Update HTML_to_ChatGPT.py

--- HTML_to_ChatGPT.py
+++ HTML_to_ChatGPT.py
@@ -13,7 +13,7 @@ def main():
         chunks = [html_code[i:i+chunk_size] for i in range(0, len(html_code), chunk_size)]
         with open("all_parts.txt", "w") as f:
             for i, chunk in enumerate(chunks):
-                f.write(f"Chunk {i+1}: {chunk} \n Wait for next chunk.")
+                f.write(f"Chunk {i+1}: {chunk}  !!Wait for next chunk.!!\מ")
             #f.write(f"Go!")
 
         st.success("HTML code has been splitted!")
